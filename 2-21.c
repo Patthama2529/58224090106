8b6a44258d9e1fc6a5ffd14619fb6674d5ef6bae
Patthama2529 <31381765+Patthama2529@users.noreply.github.com>
2017-08-27 14:43:05 +0700

    Initial commit

---
+++ 2-21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf ("My name is : %2s\n" ,"Kmitl");
+	printf ("My point : %d\n",10+40+49);
+	printf ("Grade : %c\n",'A');
+	printf ("GPA : %2f",3.99);
+	printf ("n = %c",78);
+	printf 	("%d-%d-%d",02,04,2529); // 02-04-2529
+	return 0;
+}
