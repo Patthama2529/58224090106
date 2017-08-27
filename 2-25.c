b9a8e7652418ef56ba6ab7e1c9128bdcd6d2640e
Patthama2529 <31381765+Patthama2529@users.noreply.github.com>
2017-08-27 16:45:55 +0700

    add program

--- 2-25.c
+++ 2-25.c
@@ -3,6 +3,6 @@ int main()
 {
 	printf ("Subject : %s(%d)\n","Programming",2552);
 	printf ("Point : %d\nGrade : %c",99,'A');
-	printf ("19:%x 31:%x",19,31);
+	printf ("\n19:%x 31:%x",19,31);
 	return 0;
 }
---
+++ 2-38.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	printf("% 1s\n","58224090106");
+	printf("% 1s\n","Patthama Chanwan");
+	printf("% 1s\n","02/04/1986");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("%120s\n","http://www.kru.ac.th");
+	printf("\n");
+	printf("%57 s%s","",",BYE BYE");
+	return 0;
+}
